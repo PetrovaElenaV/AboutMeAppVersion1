f0677e2f70caa598f0733a28fe6e06295141d7d0
Elena Petrova <e_v_b@mail.ru>
2023-09-23 17:22:46 +0300

    Прога после разбор от Алексея ДЗ 2.6

---
+++ AboutMeAppVersion1/ViewControllers/LoginViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  AboutMeAppVersion1
+//
+//  Created by Elena Petrova on 06.09.2023.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private Properties
+    private let user = "1"
+    private let password = "1"
+    
+    // MARK: - Override Methods
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.user = user
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func logInPressed() {
+        guard userNameTextField.text == user, passwordTextField.text == password else {
+            showAlert(
+                title: "🥹 Invalid login or password",
+                message: "🤝 Please, enter correct login and password",
+                textField: passwordTextField
+            )
+            return
+        }
+        performSegue(withIdentifier: "openWelcomeVC", sender: nil)
+    }
+    
+    @IBAction func forgotRegisterData(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Oops!⛔️", message: "Your name is ✓ \(user)")
+        : showAlert(title: "Oops! 📛 ", message: " Your password is ✓ \(password)")
+    }
+    
+    @IBAction func unwindSegue(for segue: UIStoryboardSegue) {
+        userNameTextField.text = ""
+        passwordTextField.text = ""
+    }
+    
+    // MARK: - UIAlertControllers
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "✅ OK", style: .default) { _ in
+            textField?.text = ""
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
---
+++ AboutMeAppVersion1/ViewControllers/WelcomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  InfoViewController.swift
+//  AboutMeAppVersion1
+//
+//  Created by Elena Petrova on 10.09.2023.
+//
+
+import UIKit
+
+final class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var userLabel: UILabel!
+    
+    var user = ""
+    
+    private let primaryColor = UIColor(
+        displayP3Red: 64/255,
+        green: 224/255,
+        blue: 208/255,
+        alpha: 1
+    )
+    private let secondaryColor = UIColor(
+        displayP3Red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        userLabel.text = "\(user) !   🤍 "
+    }
+}
+// MARK: - Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
+}
